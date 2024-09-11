293b5c69e1f59e895dce89b384ddcdd4d3095da8
RobinSoGood <dagcat@mail.ru>
2024-09-11 19:18:36 +0300

    iter4

--- cmd/shortener/config.go
+++ cmd/shortener/config.go
@@ -14,7 +14,7 @@ func parseFlags() {
 	// регистрируем переменную flagRunAddr
 	// как аргумент -a со значением :8080 по умолчанию
 	flag.StringVar(&flagRunAddrA, "a", ":localhost:8080", "address and port to run server")
-	flag.StringVar(&flagRunAddrB, "a", "https://localhost:8080", "address and port to run server")
+	flag.StringVar(&flagRunAddrB, "b", "https://localhost:8080", "address and port to run server")
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
 }
